6604299a002553c8e18fdfe2069026c8fad56a5f
LesKukh <73469139+LesKukh@users.noreply.github.com>
2020-12-23 19:20:43 +0200

    Update PrivateRoute.js
    commnets added

--- members-only-app/src/auth/PrivateRoute.js
+++ members-only-app/src/auth/PrivateRoute.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
-export const PrivateRoute = ({ isAuthed, isLoading, ...props }) => {
+export const PrivateRoute = ({ isAuthed, isLoading, ...props }) => {//gatehr props into a spread parameter
     if (isLoading) {
       return <div>Loading...</div>;
     }
-  
+  //if user is not authorized redirect to Sign In page
     if (!isAuthed) {
       return <Redirect to='/sign-in' />
     }
-  
+
     return (
-      <Route {...props} />
+      <Route {...props} />//if loaded and authorized pass spreaded props to Route
     );
 }
