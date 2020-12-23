3a19d2ebd762795831e95171758d1f54c8c881aa
LesKukh <73469139+LesKukh@users.noreply.github.com>
2020-12-23 19:44:19 +0200

    Update useUser.js
    comments added to useUser custom hook

--- members-only-app/src/auth/useUser.js
+++ members-only-app/src/auth/useUser.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 
 export const useUser = () => {
-    const [userInfo, setUserInfo] = useState(() => {
+    const [userInfo, setUserInfo] = useState(() => {//pass function to useState that returns userInfo object
         const user = firebase.auth().currentUser;
         const isLoading = !user;
         return { isLoading, user };
