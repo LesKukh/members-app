5d5ac4174a1a0e54999540714a84fceaba363adc
LesKukh <73469139+LesKukh@users.noreply.github.com>
2020-12-24 13:19:48 +0200

    Update GroupsList.js

--- members-only-app/src/groups/GroupsList.js
+++ members-only-app/src/groups/GroupsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+//ListItemComponent is passed as a prop which is set to a value in the
+//GroupListPage component
 export const GroupsList = ({ isLoading, groups, ListItemComponent }) =>
     isLoading
         ? <p>Loading...</p>
