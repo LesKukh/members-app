408d3d5d7b89fa5be6a52b4441c586759d2a2348
LesKukh <73469139+LesKukh@users.noreply.github.com>
2020-12-24 09:58:52 +0200

    Update useUser.js

--- members-only-app/src/auth/useUser.js
+++ members-only-app/src/auth/useUser.js
@@ -3,13 +3,15 @@ import firebase from 'firebase/app';
 
 export const useUser = () => {
     const [userInfo, setUserInfo] = useState(() => {//pass function to useState that returns userInfo object
+      //is used since the initial state needs to be calculated
         const user = firebase.auth().currentUser;
         const isLoading = !user;
         return { isLoading, user };
     });
 
     useEffect(() => {
-        return firebase.auth().onAuthStateChanged(user => {
+        return firebase.auth().onAuthStateChanged(user => {//onAuthStateChanged will call its
+          //callback function when the compoent is unmounted to unsubscribe the user
             setUserInfo({ isLoading: false, user });
         });
     }, []);
