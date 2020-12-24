cd9a0cae609c9800d62a4894780e1b530eb49c6b
LesKukh <73469139+LesKukh@users.noreply.github.com>
2020-12-24 12:26:55 +0200

    Update RequestsListItem.js

--- members-only-app/src/requests/RequestsListItem.js
+++ members-only-app/src/requests/RequestsListItem.js
@@ -3,6 +3,8 @@ import React from 'react';
 export const RequestsListItem = ({ request, onAccept, onReject }) => (
     <div>
         <h3>{request.userName}</h3>
+        //wrap event handlers with arrow functions so that they can accept params and not
+        //being called every time the component renders
         <button onClick={() => onAccept(request.id)}>Accept</button>
         <button onClick={() => onReject(request.id)}>Reject</button>
     </div>
